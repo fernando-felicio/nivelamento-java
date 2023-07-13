b39cf1607c006e154efd88f36a1ba60d44b71159
Fernando Felício <113732116+fernando-felicio@users.noreply.github.com>
2023-07-13 10:59:50 -0300

    Update uri1038.java
    Declarando as variaveis do produto, não seria caracterizado como uma boa prática?

--- src/uri1038.java
+++ src/uri1038.java
@@ -4,7 +4,7 @@ public class uri1038 {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 
 		int codigo = sc.nextInt();
 		int quantidade = sc.nextInt();
@@ -27,6 +27,41 @@ public class uri1038 {
 		}
 
 		System.out.printf("Total: R$ %.2f%n", total);
+		
+		sc.close();*/
+
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		int quantidade, codigo;
+		double cachorroQuente, xSalada, xBacon, torradaSimples, refrigerante, valorTotal;
+		
+		
+		cachorroQuente = 4.00;
+		xSalada = 4.50;
+		xBacon = 5.00;
+		torradaSimples = 2.00;
+		refrigerante = 1.50;
+		
+		System.out.println("Digite o código do produto:");
+		codigo = sc.nextInt();
+		System.out.println("Digite a quantidade que deseja comprar:");
+		quantidade  = sc.nextInt();
+		
+		if (codigo == 1) {
+			valorTotal = cachorroQuente * quantidade;
+		}else if (codigo == 2) {
+			valorTotal = xSalada * quantidade;
+		}else if (codigo == 3) {
+			valorTotal = xBacon * quantidade;
+		}else if (codigo == 4) {
+			valorTotal = torradaSimples * quantidade;
+		}
+		else {
+			valorTotal = refrigerante * quantidade;
+		}
+		
+		System.out.printf("O valor total da sua compra é de: R$%.2f", valorTotal);
 		
 		sc.close();
 	}
